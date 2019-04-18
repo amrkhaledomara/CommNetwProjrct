4d22277c4f3e69c6492d84fa2d0f450b5966ca15
amrkhaledomara <49492305+amrkhaledomara@users.noreply.github.com>
2019-04-18 09:33:18 +0200

    Add files via upload
    milestone 2

--- TCPClient.java
+++ TCPClient.java
@@ -1,33 +1,82 @@
 package TCPClient;
-import java.io.*; 
-import java.net.*; 
-public class TCPClient { 
 
-    public static void main(String argv[]) throws Exception 
-    { 
-        String sentence; 
-        String modifiedSentence; 
-
-        BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
-
-        Socket clientSocket = new Socket("localhost", 6789); 
-
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
-        BufferedReader inFromServer = 
-                new BufferedReader(new
-                InputStreamReader(clientSocket.getInputStream())); 
-
-              sentence = inFromUser.readLine(); 
-
-              outToServer.writeBytes(sentence + '\n'); 
-
-              modifiedSentence = inFromServer.readLine(); 
-
-              System.out.println("FROM SERVER: " + modifiedSentence); 
-
-              clientSocket.close(); 
-                         
-          } 
-      } 
+import java.io.*;
+import java.net.*;
+public class TCPClient {
+	public static void main(String[] args)
+	{
+		BufferedReader brinput;
+		while(true){
+			System.out.println("Type 'CONNECT' to connect to server");
+			brinput = new BufferedReader(new InputStreamReader(System.in));
+			String msgtoServerString=null;
+			try {
+				msgtoServerString = brinput.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(msgtoServerString.equals("CONNECT"))
+			break;
+			}
+		
+		try {
+			Socket sock = new Socket("localhost",6789);
+			SendThread sendThread = new SendThread(sock);
+			Thread thread = new Thread(sendThread);thread.start();
+			RecieveThread recieveThread = new RecieveThread(sock);
+			Thread thread2 =new Thread(recieveThread);thread2.start();
+		} catch (Exception e) {System.out.println(e.getMessage());} 
+	}
+}
+class RecieveThread implements Runnable
+{
+	Socket sock=null;
+	BufferedReader recieve=null;
+	
+	public RecieveThread(Socket sock) {
+		this.sock = sock;
+	}
+	public void run() {
+		try{
+		recieve = new BufferedReader(new InputStreamReader(this.sock.getInputStream()));
+		String msgRecieved = null;
+		while((msgRecieved = recieve.readLine())!= null)
+		{
+			System.out.println("From Server: " + msgRecieved);
+			System.out.println("Please enter something to send to server..");
+		}
+		}catch(Exception e){System.out.println(e.getMessage());}
+	}
+}
+class SendThread implements Runnable
+{
+	Socket sock=null;
+	PrintWriter print=null;
+	BufferedReader brinput=null;
+	
+	public SendThread(Socket sock)
+	{
+		this.sock = sock;
+	}
+	public void run(){
+		try{
+		if(sock.isConnected())
+		{
+			System.out.println("Client connected to "+sock.getInetAddress() + " on port "+sock.getPort());
+			this.print = new PrintWriter(sock.getOutputStream(), true);	
+		while(true){
+			System.out.println("Type your message to send to server..type 'bye' to exit");
+			brinput = new BufferedReader(new InputStreamReader(System.in));
+			String msgtoServerString=null;
+			msgtoServerString = brinput.readLine();
+			this.print.println(msgtoServerString);
+			this.print.flush();
+		
+			if(msgtoServerString.equals("bye"))
+			break;
+			}
+		sock.close();}}catch(Exception e){System.out.println(e.getMessage());}
+	}
+}
--- TCPServer.java
+++ TCPServer.java
@@ -1,32 +1,77 @@
 package TCPServer;
-
-import java.io.*; 
-import java.net.*; 
-
-class TCPServer { 
-
-  public static void main(String argv[]) throws Exception 
-    { 
-      String clientSentence; 
-      String capitalizedSentence; 
-
-      ServerSocket welcomeSocket = new ServerSocket(6789); 
-  
-      while(true) { 
-  
-            Socket connectionSocket = welcomeSocket.accept(); 
-
-           BufferedReader inFromClient = 
-              new BufferedReader(new
-              InputStreamReader(connectionSocket.getInputStream())); 
-           DataOutputStream  outToClient = 
-                   new DataOutputStream(connectionSocket.getOutputStream()); 
-
-                 clientSentence = inFromClient.readLine(); 
-
-                 capitalizedSentence = clientSentence.toUpperCase()+ '\n'; 
-
-                 outToClient.writeBytes(capitalizedSentence); 
-              } 
-          } 
-      } 
+import java.io.*;
+import java.net.*;
+public class TCPServer {
+	public static void main(String[] args) throws IOException {
+		final int port = 6789;
+		System.out.println("Server waiting for connection on port "+port);
+		ServerSocket ss = new ServerSocket(port);
+		Socket clientSocket = ss.accept();
+		System.out.println("Recieved connection from "+clientSocket.getInetAddress()+" on port "+clientSocket.getPort());
+		
+		RecieveFromClientThread recieve = new RecieveFromClientThread(clientSocket);
+		Thread thread = new Thread(recieve);
+		thread.start();
+		SendToClientThread send = new SendToClientThread(clientSocket);
+		Thread thread2 = new Thread(send);
+		thread2.start();
+	}}
+class RecieveFromClientThread implements Runnable
+{
+	Socket clientSocket=null;
+	BufferedReader brBufferedReader = null;
+	
+	public RecieveFromClientThread(Socket clientSocket)
+	{
+		this.clientSocket = clientSocket;
+	}//end constructor
+	public void run() {
+		try{
+		brBufferedReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));		
+		
+		String messageString;
+		while(true){
+		while((messageString = brBufferedReader.readLine())!= null){
+			if(messageString.equals("EXIT"))
+			{
+				break;
+			}
+			System.out.println("From Client: " + messageString);
+			System.out.println("Please enter something to send back to client..");
+		}
+		this.clientSocket.close();
+		System.exit(0);
+	}
+		
+	}
+	catch(Exception ex){System.out.println(ex.getMessage());}
+	}
+}
+class SendToClientThread implements Runnable
+{
+	PrintWriter pwPrintWriter;
+	Socket clientSock = null;
+	
+	public SendToClientThread(Socket clientSock)
+	{
+		this.clientSock = clientSock;
+	}
+	public void run() {
+		try{
+		pwPrintWriter =new PrintWriter(new OutputStreamWriter(this.clientSock.getOutputStream()));
+		
+		while(true)
+		{
+			String msgToClientString = null;
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			
+			msgToClientString = input.readLine();
+			
+			pwPrintWriter.println(msgToClientString);
+			pwPrintWriter.flush();
+			System.out.println("Please enter something to send back to client..");
+		}
+		}
+		catch(Exception ex){System.out.println(ex.getMessage());}	
+	}
+}
